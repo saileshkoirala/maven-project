9646704730a4ae0ec4dfa42a0c3137a04485942d
saileshkoirala <sailesh.koirala@gmail.com>
2020-04-10 15:51:19 +0545

    adding unit testing

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -2,9 +2,14 @@ package clinic.programming.training;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.commons.lang3.StringUtils;
 
 public class Application {
 
+    public int countWords(String words){
+    	String[] separateWords = StringUtils.split(words, ' ');
+    	return (separateWords == null) ? 0 : separateWords.length;
+	}
 	public void Hello()
 	{
 		List<String> greetings = new ArrayList<>();
@@ -21,8 +26,10 @@ public class Application {
 
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
-     	System.out.println ("Starting Application");
+     	System.out.println("Starting Application");
 		Application app = new Application();
 		app.Hello();
+		int count = app.countWords("I have four words");
+		System.out.println("Word Count: " + count);  
     }
 }
